be219a857b9424bf9ed50a9ec9cf50a897ac6799
Brett Spradbury <brett.spradbury@gmail.com>
2025-07-31 12:31:53 -0300

    general fixes

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # chat-gpt-bot
 An interactive chat bot using a local GPT model
+
+## Instructions
+1. start your docker daemon:  
+`e.g. Docker Desktop, or Orbstack, etc.`
+
+2. build the container:  
+`npm run docker:build`
+
+3. start the docker services:  
+`npm run docker`
+
+4. open the webpage & start using the GPT:  
+[http://0.0.0.0:5173](http://0.0.0.0:5173)
--- docker/services/vue/vite.config.js
+++ docker/services/vue/vite.config.js
@@ -1,14 +1,12 @@
 import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
 import Components from 'unplugin-vue-components/vite'
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   plugins: [
     vue(),
-    vueDevTools(),
     tailwindcss(),
     Components({
       dirs: ['src/components']
